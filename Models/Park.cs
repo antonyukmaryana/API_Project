eae5ed2f5c7fec42405816eaa60498395051384c
maryanaantonyuk <amaryana@gmail.com>
2019-09-08 16:14:22 -0700

    Initial commit

---
+++ Models/Park.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ParksAPI.Models
+{
+    [Table("parks")]
+    public class Park
+    {
+        [Key]
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
---
+++ Models/ParkAPI.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksAPI.Models
+{
+    public class ParksAPIContext : DbContext
+    {
+        public DbSet<Park> Parks {get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"server=localhost;user id=root;password=mar241871;port=3306;database=parks_api;");
+
+        public ParksAPIContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public ParksAPIContext()
+        {
+
+        }
+    }
+}
